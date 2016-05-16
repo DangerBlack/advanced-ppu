f6fab84b728b3014818b9c2e78c55d5c62219fc1
DangerBlack <danger.recheng@hotmail.it>
2016-05-16 23:42:02 +0200

    Errore di privilegio

--- php/updateUserInfo.php
+++ php/updateUserInfo.php
@@ -13,14 +13,14 @@
 	$email=$_POST['email'];
 	$ruolo=$_POST['ruolo'];/*check privilege escalation*/
 	$branca=$_POST['branca'];
-	if(($id==getId())&&(getRuolo()<=$ruolo)){//Mi sto cambiando il privilegio da solo (posso solo scendere)
+	if(($id==getId())&&(getRuolo()<=$ruolo)){//Mi sto cambiando il privilegio da solo (posso solo scendere di privilegio (numeri più grandi))
 		if(updateUserInfo($id,$utente,$email,$ruolo,$branca)!=0)
 			echo 202;
 		else
 			echo 500;
 	}else{
-		$ruolo_originale=getUser()[0]['ruolo'];
-		if(($id!=getId())&&(getRuolo()<=$ruolo)&&($ruolo>=$ruolo_originale)){//sto cambiando il privilegio ad un altro posso solo farlo aumentare fino al mio livello
+		$ruolo_originale=getUser($id)[0]['ruolo'];
+		if(($id!=getId())&&(getRuolo()<=$ruolo)&&(($ruolo<=$ruolo_originale)||(getRuolo()==0))){//sto cambiando il privilegio ad un altro posso solo farlo aumentare fino al mio livello (numeri più piccoli)
 			if(updateUserInfo($id,$utente,$email,$ruolo,$branca)!=0)
 				echo 202;
 			else
